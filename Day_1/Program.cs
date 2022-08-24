dc7edcaf55526b37e76fe6b76271f0b09eaeb3ec
Cy8rax <cybralex@gmail.com>
2022-08-24 23:08:32 +1100

    New Day

---
+++ Day_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int number2= number*(-1);
+
+while(number2<=number)
+{
+    Console.WriteLine(number2);
+    number2++;
+}
