d18d7a6c8628cf3a135890ee666d0756accef3cd
Cy8rax <cybralex@gmail.com>
2022-08-25 00:06:49 +1100

    Day_2 1/2

---
+++ Day_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int rnumber = new Random().Next(10,100);
+Console.WriteLine(rnumber);
+int a = rnumber % 10;
+Console.WriteLine(a);
+int b = (rnumber)/10;
+Console.WriteLine(b);
+if(a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
