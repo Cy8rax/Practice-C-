df40bc505c63ac449c0f8174e35142c7221496ac
Cy8rax <cybralex@gmail.com>
2022-08-25 22:32:02 +1100

    Wtf

--- Day_2/Program.cs
+++ Day_2/Program.cs
@@ -20,15 +20,27 @@
 // int c = rnum%10;
 // Console.Write("{0}{1}",a,c);
 
-int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = Convert.ToInt32(Console.ReadLine());
-double a = num1 % num2;
-if(a == 0)
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int a = num1 % num2;
+// if(a == 0)
+// {
+//     Console.WriteLine("the number is Кратно ))");
+// }
+// else
+// {
+//     Console.WriteLine("The remainder: " + a);
+// }
+// Console.WriteLine(a);
+
+//Write prog that accept 2 numbers and determines that its кратно и 7 и 23:
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 == 0)
 {
-    Console.WriteLine("the number is Кратно ))");
+    Console.WriteLine("Это число кратно 7 & 23" + a);
 }
 else
 {
-    Console.WriteLine("The remainder" + a);
+    Console.WriteLine("Не кратно");
 }
-Console.WriteLine(a);
