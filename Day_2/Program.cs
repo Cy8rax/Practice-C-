228f16d794ea01a0d5e6ca43ea2ae047722774b3
Cy8rax <cybralex@gmail.com>
2022-08-25 12:21:08 +1100

    Day_2 2

--- Day_2/Program.cs
+++ Day_2/Program.cs
@@ -1,14 +1,27 @@
-﻿int rnumber = new Random().Next(10,100);
-Console.WriteLine(rnumber);
-int a = rnumber % 10;
+﻿// int rnumber = new Random().Next(10,100);
+// Console.WriteLine(rnumber);
+// int a = rnumber % 10;
+// Console.WriteLine(a);
+// int b = (rnumber)/10;
+// Console.WriteLine(b);
+// if(a>b)
+// {
+//     Console.WriteLine(a);
+// }
+// else
+// {
+//     Console.WriteLine(b);
+// }
+
+//App that delete 2nd number in 3rd Count
+// int rnum = new Random().Next(100,1000);
+// Console.WriteLine(rnum);
+// int a = rnum/100;
+// int c = rnum%10;
+// Console.Write("{0}{1}",a,c);
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+double a = num1/num2;
+
 Console.WriteLine(a);
-int b = (rnumber)/10;
-Console.WriteLine(b);
-if(a>b)
-{
-    Console.WriteLine(a);
-}
-else
-{
-    Console.WriteLine(b);
-}
