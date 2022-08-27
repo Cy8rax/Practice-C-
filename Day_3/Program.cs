9a5fd190d094aec75f023af438160db8e7024919
Cy8rax <cybralex@gmail.com>
2022-08-27 13:47:46 +1100

    End of the Day_3

--- Day_3/Program.cs
+++ Day_3/Program.cs
@@ -3,3 +3,57 @@
 
 // Console.WriteLine(str[2]);
 
+//Прога определяющая в какой плоскости находится точка: 
+// Console.WriteLine("Enter X: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// if(x>0&&y>0)
+// {
+//     Console.WriteLine("1");
+// }
+// if(x<0&&y>0)
+// {
+//     Console.WriteLine("2");
+// }
+// if(x<0&&y<0)
+// {
+//     Console.WriteLine("3");
+// }
+// if(x>0&&y<0)
+// {
+//     Console.WriteLine("4");
+// }
+//Написать программу которая принимает координаты 2х точек и выводит расстояние между ними.
+// Console.WriteLine("Enter A(x): ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter A(y): ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter B(x): ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter B(y): ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// double dist = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
+// Console.WriteLine(dist);
+
+//Написать таблицу которая при вводу числа выдает квадрат всех чисел до этого числа
+
+// Console.WriteLine("Insert Number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int index = 1;
+
+// while(index<num+1)
+// {
+//     Console.Write(Math.Pow(index,2) + ", ");
+//     index++;
+// }
+
+//Example of cicrle "For..."
+int num=6;
+
+for(int i=1;i<num;i++)
+{
+    Console.WriteLine(i);
+}
